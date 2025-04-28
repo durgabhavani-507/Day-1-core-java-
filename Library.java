be178ed8795e339e439470ed95c70ce8b582be35
durgabhavani-507 <angadala507@gmail.com>
2025-04-28 17:46:43 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,9 @@
+public class Library{
+public static void displaywelcomemessage()
+{
+System.out.println(" Welcome to the Library Management System");
+}
+public static void main(String[]args){
+displaywelcomemessage();
+}
+}
---
+++ Store.java
@@ -0,0 +1,14 @@
+public class Store{
+public static void displayStoreHours()
+{
+System.out.println(" store hours:9AM-9PM");
+}
+public static void displayStoreAddress()
+{
+System.out.println(" store Address:123 main street,NY");
+}
+public static void main(String[]args){
+displayStoreHours();
+displayStoreAddress();
+}
+}
